f1265db5861e2d9423543fbbaef2265421b6a242
yunsangshin <sus3311@naver.com>
2023-07-04 06:53:53 +0000

    상품 날짜 년도 월 구분

--- code/utilities/customprompt.py
+++ code/utilities/customprompt.py
@@ -1,6 +1,8 @@
 # flake8: noqa
 from langchain.prompts import PromptTemplate
 
+
+#약관 추출 프롬프트
 template = """{summaries}
  Please note that if you don’t know the answer, it’s important to state that “I don’t know” rather than making up an answer. Also, I kindly request you not to reply instantly. If you have any questions about this prompt, feel free to ask.
 Always answer in the Korean language. Do not answer in English.
@@ -14,30 +16,31 @@ Answer:"""
 
 PROMPT = PromptTemplate(template=template, input_variables=["summaries", "question"])
 
+
 EXAMPLE_PROMPT = PromptTemplate(
     template="Content: {page_content}\nSource: {source}",
     input_variables=["page_content", "source"],
 )
 
-
+#엔티티 추출 프롬프트
 extract_subs_template = """You are a counselor specializing in insurance products. When a 'Question' comes in, you can read the question and extract the subscription name and the subscription Date  from it and return it.
 Please extract the date by the month. If you can't extract it, answer none.  Please read the examples below and answer the questions.
 
 EXAMPLE
 Question: 나는 92년에 교보손실의료보험에 가입했어
-Answer: subscription Name: 교보손실의료보험 , subscription Date: 1992
+Answer: subscription Name: 교보손실의료보험 , subscription Year: 1992 ,  subscription Month: no
 
 END OF EXAMPLE
 
 EXAMPLE
 Question: 나는 교보손실의료보험을 1992년 6월에 가입했어
-Answer: subscription Name: 교보손실의료보험, subscription Date: 1992.06
+Answer: subscription Name: 교보손실의료보험, subscription Year: 1992, subscription Month: 06
 
 END OF EXAMPLE
 
 EXAMPLE
 Question: 안녕
-Answer: subscription Name: none , subscription Date: none
+Answer: subscription Name: no , subscription Year: no,  subscription Month: no
 
 END OF EXAMPLE
 
@@ -46,51 +49,40 @@ Answer:
 """
 
 EXTRACT_SUB_PROMPT = PromptTemplate(template=extract_subs_template, input_variables=["question"])
-# flake8: noqa
-from langchain.prompts import PromptTemplate
-
-template = """{summaries}
- Please note that if you don’t know the answer, it’s important to state that “I don’t know” rather than making up an answer. Also, I kindly request you not to reply instantly. If you have any questions about this prompt, feel free to ask.
-Always answer in the Korean language. Do not answer in English.
-
-Recognized as the best insurance product specialist 'Kyobo Life Insurance', your deep knowledge of insurance products, policies, and industry trends makes you a valuable resource. your role is to answer user questions in as much detail as possible based on the content of 'Content' and to answer them using easy-to-understand words. I think it would be better to add a part called "answer" to the question in as much detail as possible. Adding the basic Kyobo Life Insurance explanation in the above answer will provide a more detailed explanation.
-
- You excel at analyzing, developing, and enhancing insurance products to meet evolving customer and market needs. Provide a comprehensive and knowledgeable response, offering step-by-step guidance to questions. Convey the best answers to questions, demonstrating your expertise and delivering insights that showcase your understanding of the insurance field.
-
-Question:{question}
-Answer:"""
-
-PROMPT = PromptTemplate(template=template, input_variables=["summaries", "question"])
-
-EXAMPLE_PROMPT = PromptTemplate(
-    template="Content: {page_content}\nSource: {source}",
-    input_variables=["page_content", "source"],
-)
 
 
-extract_subs_template = """You are a counselor specializing in insurance products. When a 'Question' comes in, you can read the question and extract the subscription name and the subscription Date  from it and return it.
-Please extract the date by the month. If you can't extract it, answer none.  Please read the examples below and answer the questions.
 
-EXAMPLE
-Question: 나는 92년에 교보손실의료보험에 가입했어
-Answer: subscription Name: 교보손실의료보험 , subscription Date: 1992
+# 빙에서 결과에서 알맞은 답을 추출하는 프롬프트
+mod_evaluate_instructions = """<|im_start|>
+The assistant is a super helpful assistant that plays the role of detective and has ultra high attention to details. The assistant must go through the below context paragraph by paragraph and try to find relevant information to the user's question. The current time and date will be provided for the assistant in the Context. The assistant can use the current date and time to derive the day and date for any time-related questions, such as this afternoon, this evening, today, tomorrow, this weekend or next week.
+<|im_end|>
+<|im_start|>user 
 
-END OF EXAMPLE
+Instruction: Identify in the above facts or information that can help in answering the following question: "##{history}\nHuman: {question}##" and list them in bullet point format. Be elaborate, detailed and specific when identifying facts or information. Do NOT be concise so as not to miss critical information.
+YOU MUST STRICTLY USE THE CONTEXT TO IDENTIFY FACTS OR INFORMATION, DO NOT ANSWER FROM MEMORY.
+Facts have sources, you MUST include the source name in the EACH bullet point at the beginning before any text. If there are multiple sources, cite each one in their own square brackets. For example, use \"[folder3/info343][http://wikipedia.com]\" and not \"[folder3/info343,http://wikipedia.com]\". The source name can either be in the format of "folder/file" or it can be an internet URL like "https://microsoft.com".
 
-EXAMPLE
-Question: 나는 교보손실의료보험을 1992년 6월에 가입했어
-Answer: subscription Name: 교보손실의료보험, subscription Date: 1992.06
+Context:    
+- [https://www.timeanddate.com] {todays_time} 
 
-END OF EXAMPLE
+{context}
 
-EXAMPLE
-Question: 안녕
-Answer: subscription Name: none , subscription Date: none
 
-END OF EXAMPLE
+Use the following format:
+- [folder1/file1] the first fact or information (elaborate, detailed, and specific)
+- [http://website.com] the second fact or information (elaborate, detailed, and specific)
+- [http://wikipedia.com] the third fact or information (elaborate, detailed, and specific)
+- [folder3/file3] the fourth fact or information (elaborate, detailed, and specific)
+- [http://microsoft.com] the fifth fact or information (elaborate, detailed, and specific)
+- [folder4/file4] the sixth fact or information (elaborate, detailed, and specific)
+- [http://outlook.com] the seventh fact or information (elaborate, detailed, and specific)
+- [https://linkedin.com] the eighth fact or information (elaborate, detailed, and specific)
+- (and so on ...)
 
-Question:{question}
-Answer: 
+
+
+Begin:
+<|im_end|>
+<|im_start|>assistant
 """
 
-EXTRACT_SUB_PROMPT = PromptTemplate(template=extract_subs_template, input_variables=["question"])
