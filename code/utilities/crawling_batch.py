bde53c345c0ffa2357653022360b922893c1e97a
soulFreeOneKim <soulfree89@naver.com>
2023-07-12 17:57:07 +0900

    크롤링 수정 중 for 중간저장

--- code/utilities/crawling_batch.py
+++ code/utilities/crawling_batch.py
@@ -15,14 +15,20 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import Select, WebDriverWait
+from selenium.common.exceptions import StaleElementReferenceException
 
+
+
+MAX_RETRY = 5
+retry_count = 0
 
 chrome_options = Options()
-# chrome_options.add_argument('headless') #headless모드 브라우저가 뜨지 않고 실행됩니다.
+chrome_options.add_argument('headless') #headless모드 브라우저가 뜨지 않고 실행됩니다.
 # chrome_options.add_argument('--window-size= x, y') #실행되는 브라우저 크기를 지정할 수 있습니다.
-chrome_options.add_argument('--start-maximized') #브라우저가 최대화된 상태로 실행됩니다.
+# chrome_options.add_argument('--start-maximized') #브라우저가 최대화된 상태로 실행됩니다.
 # chrome_options.add_argument('--start-fullscreen') #브라우저가 풀스크린 모드(F11)로 실행됩니다.
 # chrome_options.add_argument('--blink-settings=imagesEnabled=false') #브라우저에서 이미지 로딩을 하지 않습니다.
 # chrome_options.add_argument('--mute-audio') #브라우저에 음소거 옵션을 적용합니다.
@@ -59,17 +65,6 @@ def autoInstallerChromeDriver():
         print(f"install the chrome driver(ver: {chrome_ver})")
         chromedriver_autoinstaller.install(True)
 
-# # 4. 팝업 화면 컨트롤
-# def change_window(self, target: str):
-#     if target == 'parent':
-#         # child window close
-#         self.driver.close()
-#         self.driver.switch_to.window(self.driver.window_handles[0])
-#     elif target == 'child':
-#         self.driver.switch_to.window(self.driver.window_handles[1])
-#     else:
-#         print("Wrong target!")
-
 
 # 현재 열려진 크롬에서 실행하기
 openChromeSelenium(False)
@@ -82,33 +77,15 @@ setDownLoadPath("down_load")
 autoInstallerChromeDriver()
 logging.info(f"driver_path : {driver_path}")
 
-
-
-# 브라우저 실행 및 탭 추가
-# browser.execute_script('window.open("about:blank", "_blank");')
-# browser.execute_script('window.open("about:blank", "_blank");')
-# browser.execute_script('window.open("about:blank", "_blank");')
-
-# tabs = browser.window_handles
-
-
-# # TAB_1
-# browser.switch_to.window(tabs[0])
-# browser.get('http://www.naver.com/')
-
-# # 종료하기
-# time.sleep(5)
-
-# pyautogui.alert('종료합니다.')
-# browser.quit()
-
 driver = webdriver.Chrome(options=chrome_options)
-
-# 웹사이트 접근
 driver.get('https://www.kyobo.com/dgt/web/product-official/all-product/search')
+driver.implicitly_wait(time_to_wait=10)
 
 # 검색 조건 입력
 select_box = Select(driver.find_element(By.ID, 'select-11'))
+select_box.select_by_value("99") # 전체
+
+select_box = Select(driver.find_element(By.ID, 'select-12'))
 select_box.select_by_value("99") # 전체
 
 select_box = Select(driver.find_element(By.ID, 'select-12'))
@@ -121,79 +98,147 @@ select_box.select_by_value("100")
 search_button = driver.find_element(By.ID,'searchBtn')  # 검색 버튼의 id 속성에 맞게 수정하세요.
 search_button.send_keys(Keys.ENTER)
 
-time.sleep(1.5)
 
-# 'insuList'라는 id를 가진 tbody 요소 찾기
-tbody = driver.find_element(By.ID, 'insuList')
+wait_product_list_table = \
+    WebDriverWait(driver,10).until(expected_conditions.visibility_of_element_located((By.CLASS_NAME, 'tblist')))
+m_td = driver.find_element(By.CLASS_NAME, 'tblist')
 
-# tbody 내의 모든 tr 요소 찾기
-tr = tbody.find_elements(By.TAG_NAME, 'tr')
+wait_insuList = \
+    WebDriverWait(driver,10).until(expected_conditions.visibility_of_element_located((By.XPATH, '//*[@id="insuList"]')))
+m_insuList = m_td.find_element(By.ID, 'insuList')
+print(f"m_insuList : {m_insuList}")
 
-# 정규 표현식으로 패턴 찾기
-pattern = r'1000\d{3}'
+wait_tr = \
+    WebDriverWait(driver,10).until(expected_conditions.presence_of_all_elements_located((By.TAG_NAME, 'tr')))
+m_tr = m_insuList.find_elements(By.TAG_NAME, 'tr')
 
-# 각 행에 대해 반복
+time.sleep(0.5)
 
-count = 1
-for row in tr:
-    count += 1
-    # 각 행의 모든 td 요소 찾기
-    td = row.find_elements(By.TAG_NAME, 'td')
-    # 각 셀에 대해 반복
-    for idx, cell in enumerate(td):
-        if idx !=0 and idx%3 == 0:
+for row in m_tr:
+
+    time.sleep(0.5)
+    print("before get m_dt")
+    retry_count = 0
+    while retry_count < MAX_RETRY:
+        try:
+            m_td = row.find_elements(By.TAG_NAME, 'td')
+            # 필요한 작업을 수행합니다.
+            break
+        except StaleElementReferenceException:
+            retry_count += 1
+            print("for get m_dt --- retry")
+            time.sleep(1)  # 잠시 대기
+       
+    print("after get m_dt")
+
+    for idx, cell in enumerate(m_td):
+        if idx == 3:
             # 셀의 텍스트 출력
-            print(cell.text)
+            # print(cell.text)
             print(cell.get_attribute('innerHTML'))
-
+            pattern = r'1000\d{3}'
             match = re.search(pattern, cell.get_attribute('innerHTML'))
             if match:
-                print(match.group())  # '1000234'
+                is_pdt_cd = match.group()                
             else:
                 print("No match found")
-            
-            download_button = cell.find_element(By.TAG_NAME,'button')
-            download_button.send_keys(Keys.ENTER) 
+                continue
+
+            time.sleep(0.2)
+
+            WebDriverWait(driver,10).until(expected_conditions.element_to_be_clickable((By.CSS_SELECTOR,'.btn.sm')))
+            download_button = cell.find_element(By.CSS_SELECTOR,'.btn.sm')
+            download_button.send_keys(Keys.ENTER)
+            #download_button.click()
+
             # 모든 창의 핸들을 가져옴
-            window_handles = driver.window_handles
+            # window_handles = driver.window_handles
 
             # 새로 열린 창(팝업)으로 제어 이동
-            driver.switch_to.window(window_handles[-1])
+            # driver.switch_to.window(window_handles[-1])
+            # //*[@id="pop-period-down"]/div/div[2]/div[3]/table
+            retry_count = 0
+            while retry_count < MAX_RETRY:
+                try:
+                    child_tbody = WebDriverWait(driver,10).until(expected_conditions.visibility_of_element_located((By.XPATH, '//*[@id="pop-periodDownList"]')))
+
+                    # 필요한 작업을 수행합니다.
+                    break
+                except StaleElementReferenceException:
+                    retry_count += 1
+                    print("for get child_tbody --- retry")
+                    time.sleep(1)  # 잠시 대기
             
-            child_tbody = driver.find_element(By.ID, 'pop-periodDownList')
-            child_tr = child_tbody.find_elements(By.TAG_NAME, 'tr')
+            #child_tbody = WebDriverWait(driver,10).until(expected_conditions.visibility_of_element_located((By.XPATH, '//*[@id="pop-periodDownList"]')))
+            #child_tbody = driver.find_element(By.ID, 'pop-periodDownList')
+            retry_count = 0
+            while retry_count < MAX_RETRY:
+                try:
+                    child_tr = child_tbody.find_elements(By.TAG_NAME, 'tr')
+                    # 필요한 작업을 수행합니다.
+                    break
+                except StaleElementReferenceException:
+                    retry_count += 1
+                    print("for get child_tr --- retry")
+                    time.sleep(1)  # 잠시 대기
 
-            for product_history in child_tr:
-                print(product_history.text)
-                product_history.find_elements(By.TAG_NAME, 'a')            
-
-                #url 인코딩
-                #"1267683303497_(무)119생활보험(98.04.01).pdf" 
-                #"1267683303497_(%EB%AC%B4)119%EC%83%9D%ED%99%9C%EB%B3%B4%ED%97%98(98.04.01).pdf" 
-
-                # from urllib.parse import quote, unquote
-
-                # s = "1267683303497_(무)119생활보험(98.04.01).pdf"
-                # encoded = quote(s)
-                # decoded = unquote(encoded)
-
-                # print(f"Original: {s}")
-                # print(f"Encoded: {encoded}")
-                # print(f"Decoded: {decoded}")
-
-
-            time.sleep(3)
-
-            # 이제 driver는 새로 열린 창(팝업)을 제어합니다.
-            # 원래 창으로 돌아가려면 다음과 같이 할 수 있습니다.
-            # driver.switch_to.window(window_handles[0])
             
-    if count == 5:
-        break
+
+            for row_history in child_tr:
+                retry_count = 0
+                while retry_count < MAX_RETRY:
+                    try:
+                        child_td = row_history.find_elements(By.TAG_NAME, 'td') 
+
+                        # 필요한 작업을 수행합니다.
+                        break
+                    except StaleElementReferenceException:
+                        retry_count += 1
+                        print("for get child_td --- retry")
+                        time.sleep(1)  # 잠시 대기
+
+
+                for idx, product_history in  enumerate(child_td):
+                    if idx == 0:
+                        history_str = product_history.text.split("~")
+                        start_date = history_str[0].strip().replace("-", "")
+                        end_date = history_str[1].strip().replace("-", "")     
+                        print(f"start_date : {start_date}")                   
+                        print(f"end_date : {end_date}")
+                        print(f"is_pdt_cd : {is_pdt_cd}")
+
+                    if idx == 1:                        
+                        ico_file_button = product_history.find_element(By.TAG_NAME,'a')
+                        href_value = ico_file_button.get_attribute('href')      
+                        # ico_file_button.send_keys(Keys.ENTER)                     
+                        match = re.search(r'"(.+)"', href_value)
+                        pdf_filename = ""
+                        if match:
+                            pdf_filename = match.group(1)
+                        else:
+                            print("No match found : file name")
+                            continue
+
+                        # API 호출
+                        # https://www.kyobo.com/file/ajax/download?fName=/dtc/pdf/mm/파일명
+                        # 아래 잠시만
+                        # donwload_url = f"https://www.kyobo.com/file/ajax/download?fName=/dtc/pdf/mm/"
+                        # donwload_url = donwload_url + pdf_filename
+                        # response = requests.get(donwload_url)
+                        
+                        # # PDF 파일로 저장
+                        # with open(f"{is_pdt_cd}-{start_date}-{end_date}.pdf", "wb") as f:
+                        #     f.write(response.content)   
+
+            child_x_button = WebDriverWait(driver,10).until(expected_conditions.element_to_be_clickable((By.XPATH, '//*[@id="pop-period-down"]/div/button')))
+            # child_x_button = driver.find_element(By.XPATH, '//*[@id="pop-period-down"]/div/button')
+            child_x_button.send_keys(Keys.ENTER) 
+
+            time.sleep(0.5)
+            
+            #driver.switch_to.window(window_handles[0])                
+          
+    # if count == 3:
+    #     break
 
 pyautogui.alert('종료합니다.')
-
-# PDF 다운로드 링크 클릭
-# download_link = driver.find_element_by_id('download-link')  # 다운로드 링크의 id 속성에 맞게 수정하세요.
-# download_link.click()
-
